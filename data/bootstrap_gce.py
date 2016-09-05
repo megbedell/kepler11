b9960bf74c3b4fde5ba7687cf4ddb1672088c568
megbedell <megbedell@gmail.com>
2016-09-05 15:45:23 -0500

    better GCE corrections

--- data/bootstrap_gce.py
+++ data/bootstrap_gce.py
@@ -3,14 +3,9 @@ import matplotlib.pyplot as plt
 import numpy as np
 from scipy.optimize import curve_fit
 import pdb
-import pickle
 
 def linear(x, m, b):
      model = m*x + b
-     return model
-
-def linbreak(x, m, offs, bk):
-     model = np.piecewise(x, [x < bk, x >= bk], [offs, lambda x: (offs + m*(x - bk))])
      return model
 
 if __name__ == "__main__":
@@ -19,18 +14,25 @@ if __name__ == "__main__":
     sun.get_data_from(data)
     K11 = q2.Star("K11")
     K11.get_data_from(data)
+    K11.get_model_atmosphere('odfnew')
     sun.get_model_atmosphere('odfnew')
     
-    # load up the posteriors and get abundances / Tc trend for the best fit solution
-    post = pickle.load(open('chainresults_K11-Sun.p','rb'))
-    K11.teff = np.median(post[:,:,0])
-    
-    K11.get_model_atmosphere('odfnew')
+    sp = q2.specpars.SolvePars()
+    sp.step_teff = 4
+    sp.step_logg = 0.04
+    sp.step_vt = 0.04
+    sp.niter = 100
+    sp.grid = 'odfnew'
+    sp.errors = True
+    q2.specpars.solve_one(K11, sp, Ref=sun)
+
     q2.abundances.one(K11, Ref=sun, silent=True, errors=False)
     sp = q2.isopars.SolvePars(key_parameter_known='logg', db='yy02.sql3', feh_offset = 0)
     pp = q2.isopars.PlotPars(make_figures=False)
-    q2.isopars.solve_one(K11, sp, PlotPars=pp, silent=True)
+    q2.isopars.solve_one(K11, sp, PlotPars=pp)
     
+    
+    # get the UNCORRECTED abundances:
     species_codes = sorted(set(K11.linelist['species']))
     species_ids = q2.abundances.getsp_ids(species_codes)
     Tc = []
@@ -52,7 +54,7 @@ if __name__ == "__main__":
             Tc = np.delete(Tc, ind[1])  #assumes there's only one other state
             species_ids = np.delete(species_ids, ind[1])
 
-    # exclude K:
+    # remove K:
     abund = np.delete(abund, 7)
     err = np.delete(err, 7)
     Tc = np.delete(Tc, 7)
@@ -64,34 +66,39 @@ if __name__ == "__main__":
     slope = np.zeros(n_iter, dtype='f16')
     intercept = np.zeros(n_iter, dtype='f16')
     # fetch some GCE quantities
-    bs = np.asarray([ q2.gce.getb(species_id) for species_id in species_ids ])
-    cs = np.asarray([ q2.gce.getc(species_id) for species_id in species_ids ])
-    Ref_age = 4.5 #Sun's age in Gyr
+    bs = np.asarray([ q2.gce.getb_linear(species_id)[0] for species_id in species_ids ])
+    b_errs = np.asarray([ q2.gce.getb_linear(species_id)[1] for species_id in species_ids ])
+    Ref_age = 4.6 #Sun's age in Gyr
     for i in range(n_iter):
-        abund_gce = abund - (np.sqrt((ages[i] - bs)**2/cs**2 + 1.0) - np.sqrt((Ref_age - bs)**2/cs**2 + 1.0))
+        b_i = np.zeros_like(bs)
+        for j in range(len(b_i)):
+            if b_errs[j] == 0:
+                continue
+            else:
+                b_i[j] = np.random.normal(bs[j], b_errs[j])
+        abund_gce = abund - (ages[i] - Ref_age)*b_i
         popt, pcov = curve_fit(linear, Tc, abund_gce, sigma=err, absolute_sigma=True)
         slope[i] = popt[0]
-        #perr = np.sqrt(np.diag(pcov))
         intercept[i] = popt[1]
         if (i % 100 == 0):
             print "{0}/{1} iterations done".format(i, n_iter)
             print "latest fit: m = {0}, b = {1}".format(slope[i], intercept[i])
     print "GCE-corrected Tc slope = {0} +/- {1}".format(np.median(slope),np.std(slope))
-    '''
     plt.clf()
-    plt.scatter(ages, slope*1.0e4)
-    plt.ylim([0.0,0.5])
+    plt.scatter(ages, slope*1.0e5)
+    plt.ylim([-6,3])
     plt.xlabel('Age (Gyr)')
-    plt.ylabel(r'$T_c$ slope after GCE correction x 10^4')
+    plt.ylabel(r'$T_c$ slope after GCE correction x 10^5')
     plt.savefig('age-Tc.png')
     plt.clf()
-    n, bins, patches = plt.hist(ages, 10, normed=1)
+    n, bins, patches = plt.hist(ages, 50, normed=1)
+    plt.plot(K11.pdf_age['x'], K11.pdf_age['y'])
     plt.xlabel('Age (Gyr)')
     plt.savefig('ages.png')
     plt.clf()
-    n, bins, patches = plt.hist(slope*1.0e4, 10, normed=1)
-    plt.xlabel(r'$T_c$ slope after GCE correction x 10^4')
+    n, bins, patches = plt.hist(slope*1.0e5, 10, normed=1)
+    plt.xlabel(r'$T_c$ slope after GCE correction x 10^5')
     plt.savefig('tc.png')
     plt.clf()
-    '''
-    np.savetxt('Tc_fits.txt',np.transpose([ages, slope, intercept]))        
+    np.savetxt('Tc_fits.txt',np.transpose([ages, slope, intercept]))
+        
